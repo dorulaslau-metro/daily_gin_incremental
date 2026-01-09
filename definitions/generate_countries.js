70201ffa39e578e7faf871097d09423a337e18b3
Doru Laslau <doru.laslau@metro-external.digital>
2026-01-09 14:01:42 +0000

    kept only UA for testing

--- definitions/generate_countries.js
+++ definitions/generate_countries.js
@@ -16,17 +16,21 @@ countries.forEach((c) => {
             `DELETE metro-bi-wb-inventory-s00.${schema_name}.gin_operational_${c.iso2}
             where DATE >= DATE_SUB(CURRENT_DATE('Europe/Bucharest'),INTERVAL 3 DAY)`
         ]
+        ,
+        tags: ["gin_operational"]
     }).query(gin_operational_XX(c));
 });
 
     publish("gin_usage", {
         type: "incremental",
         schema: schema_name,
-        dependencies: countries.map(c => `gin_operational_${c.iso2}`),
+        //dependencies: countries.map(c => `gin_operational_${c.iso2}`),
         preOperations: [
             `DELETE metro-bi-wb-inventory-s00.${schema_name}.gin_usage
             where DATE >= DATE_SUB(CURRENT_DATE('Europe/Bucharest'),INTERVAL 3 DAY)`
         ]
+        ,
+        tags: ["gin_usage"]
     }).query(gin_usage);
 
     publish("gin_daily", {
@@ -37,4 +41,6 @@ countries.forEach((c) => {
             `DELETE metro-bi-wb-inventory-s00.${schema_name}.gin_daily
             where DATE >= DATE_SUB(CURRENT_DATE('Europe/Bucharest'),INTERVAL 3 DAY)`
         ]
+        ,
+        tags: ["gin_daily"]
     }).query(gin_daily);
