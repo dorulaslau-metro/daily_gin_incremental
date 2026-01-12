bf9e289e5c1e1671ca950d974687988faffa603e
Doru Laslau <doru.laslau@metro-external.digital>
2026-01-12 14:37:07 +0000

    switched to production dataset (Country_dashboards) for production run over the night

--- includes/dataset.js
+++ includes/dataset.js
@@ -1,7 +1,7 @@
 // 2026-01-12 Doru Laslau: Sets incremental filter date scope to be used throughout the project
 // can change scope by changing this variable in file below. Eg: "..INTERVAL 1 MONTH)" etc...
 
-const dataset_name = "temp_orchestration";
+const dataset_name = "Country_dashboards";
 
 module.exports = {
     dataset_name
